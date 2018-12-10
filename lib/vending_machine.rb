a08ceb812fdbb1d4b18ce30599d826c6c5105919
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-12-10 13:43:20 +0000

    quantity changed on selection

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -23,9 +23,9 @@ class VendingMachine
 
   def select_product(item)
     selected = product_list.products[item]
-    if selected == true
-      selected[:price] < total_inserted ? @product_list.products[item][quantity] -= 1 : 'mo mney pls bbz'
-    else 
+    if selected
+      selected[:price] < total_inserted ? @product_list.products[item][:quantity] -= 1 : 'mo mney pls bbz'
+    else
       'unavailable selection'
     end
     # go into product list make sure it matches, if so check enough total inserted, 
